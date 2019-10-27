390f570475e2efeb028995e15a707ac355996b16
skuzzymiglet <skuzzymiglet@gmail.com>
2019-10-27 18:47:17 +0000

    crf rise, removed conv.sh reference

--- asl.sh
+++ asl.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-$DIR/conv.sh & 
 cpulimit -l 40 -i "$DIR/core.sh"
--- core.sh
+++ core.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 
-ffmpeg -f x11grab -s 3200x1800 -i :0.0 -c:v libvpx-vp9 -crf 20 -filter:v "setpts=0.01*PTS" -threads 1 ~/`date +screenlapse/%d_%m_%y-%H%M%S`.webm
+ffmpeg -f x11grab -s 3200x1800 -i :0.0 -c:v libvpx-vp9 -crf 15 -filter:v "setpts=0.01*PTS" -threads 1 ~/`date +screenlapse/%d_%m_%y-%H%M%S`.webm
 
